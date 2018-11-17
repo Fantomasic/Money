3d3a15d43792687becec1830e69c5e3f9a597338
Davidson228 <davidarinsh@gmail.com>
2018-11-17 10:27:16 +0200

    heroku123 1.0

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 $db = new
-\atk4\data\Persistence_SQL('mysql:dbname=money;host=localhost', 'root', '');
+\atk4\data\Persistence_SQL('mysql:dbname=heroku_33a09646a43f60a;host=eu-cdbr-west-02.cleardb.net', 'b40ba71796d5af', 'a0bf7181');
 class Client extends \atk4\data\Model {
 public $table = 'client';
 function init() {
