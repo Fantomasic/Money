9ef6ac8fa7961591db9bdafcf0312a9d8267ce91
Davidson228 <davidarinsh@gmail.com>
2018-11-24 09:36:40 +0200

    2.3

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 $db = new
 \atk4\data\Persistence_SQL('mysql:dbname=heroku_33a09646a43f60a;host=eu-cdbr-west-02.cleardb.net', 'b40ba71796d5af', 'a0bf7181');
 class Client extends \atk4\data\Model {
-public $table = 'client';
+public $table = 'client_vlad28';
 function init() {
 parent::init();
 $this->addFields(['name','phone','username']);
@@ -12,7 +12,7 @@ $this->hasMany('Friends',new Friends);
 }
 }
 class Friends extends \atk4\data\Model {
-public $table = 'friends';
+public $table = 'friends_vlad28';
 function init() {
 parent::init();
 $this->addFields(['name','phone','email']);
@@ -22,7 +22,7 @@ $this->hasMany('Refund',new Refund)->addField('total_refund', ['aggregate'=>'sum
 }
 }
 class Loan extends \atk4\data\Model {
-public $table = 'loan';
+public $table = 'loan_vlad28';
 function init() {
 parent::init();
 $this->addField('amount',['type'=>'money']);
@@ -31,7 +31,7 @@ $this->hasOne('friends_id',new Friends)->addTitle();
 }
 }
 class Refund extends \atk4\data\Model {
-public $table = 'refund';
+public $table = 'refund_vlad28';
 function init() {
 parent::init();
 $this->addField('amount',['type'=>'money']);
