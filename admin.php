0b250d0c0fcd2fc71532188bbd045d984798c98b
Davidson228 <davidarinsh@gmail.com>
2018-12-01 09:59:55 +0200

    Remake(2.6)

--- admin.php
+++ admin.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 require 'connection.php';
 $app = new \atk4\ui\App('Money');
 $app->initLayout('Centered');
-$crud=$app->add('Crud');
+$crud=$app->add('CRUD');
 $crud->setModel(new Client ($db));
-$crud=$app->add('Crud')
+$crud=$app->add('CRUD')
 $crud->setModel(new Friend ($db));
